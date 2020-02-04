f5e237fa1a272675fa7b33894c11617fc70ea188
Derya Dok <derya.dok@ogr.deu.edu.tr>
2020-02-04 19:24:19 +0300

    Updates

---
+++ precache-manifest.1a3ead363b806cad88a9271d82c95657.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c264ed52a1c541a50bfa262dccfa25a5",
+    "url": "/color-match/index.html"
+  },
+  {
+    "revision": "7c0481f989a78b483d94",
+    "url": "/color-match/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "752d9168d5a635eb8852",
+    "url": "/color-match/static/css/main.b9c1eac9.chunk.css"
+  },
+  {
+    "revision": "7c0481f989a78b483d94",
+    "url": "/color-match/static/js/2.b03ae074.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/color-match/static/js/2.b03ae074.chunk.js.LICENSE"
+  },
+  {
+    "revision": "752d9168d5a635eb8852",
+    "url": "/color-match/static/js/main.9d6f6d3e.chunk.js"
+  },
+  {
+    "revision": "90e5ea2b250f89ddcb8d",
+    "url": "/color-match/static/js/runtime-main.71412957.js"
+  }
+]);
